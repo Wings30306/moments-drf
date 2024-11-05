0cb6b2c8d37be4242e7305d6259955dad7809591
Joke Heyndels <jo_hannah@outlook.com>
2024-11-05 09:25:07 +0000

    Temporarily set DEBUG to True to get more information on error

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = "DEVELOPMENT" in os.environ
 
-DEBUG = "DEV" in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
